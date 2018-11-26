599b5e2ae71d21bbc754915e0fffbbad1f73c02e
Benjamin Quilliams <bgquilliams@gmail.com>
2018-11-26 17:29:28 -0500

    Simplified Scan to focus on network-related functions

--- src/Scan.cpp
+++ src/Scan.cpp
@@ -1,95 +1,12 @@
 #include "Scan.h"
 
 
-Scan::Scan(const std::string address, const bool verbose) {
+Scan::Scan(const std::string address) {
     this->address = address;
-    this->verbose = verbose;
-    this->numOpen = 0;
-    this->numClosed = 0;
-    this->startTime = std::chrono::system_clock::now();
 }
 
 Scan::~Scan(){ 
     //std::cout << "Destructor Called:" << this << std::endl;
-}
-
-std::string Scan::getAddress() {
-    return this->address;
-}
-
-int Scan::getNumOpen() {
-    return this->numOpen;
-}
-
-int Scan::getNumClosed() {
-    return this->numClosed;
-}
-
-std::chrono::duration<double> Scan::getElapsedTime() {
-    return this->elapsedTime;
-}
-
-
-void Scan::addPort(const int port) {
-    if(!this->portMap.count(port)){
-        PortEntry p(port);
-        bool isOpen = probe(port);
-        p.setStatus(isOpen);
-        if (isOpen) {
-            this-numOpen++;
-        }
-        else {
-            this->numClosed++;
-        }
-        this->portMap.insert(std::make_pair(port, p)); 
-    }
-}
-
-void Scan::printPorts() {
-    this->endTime = std::chrono::system_clock::now();
-    this->elapsedTime = endTime-startTime;
-    std::cout << getScanInfo() << std::endl;
-    for (auto element : this->portMap)  {
-        if (this->verbose) {     
-            std::cout << element.second.statusString() << std::endl;
-        }
-        else if (element.second.getStatus() == true) { 
-            std::cout << element.second.statusString() << std::endl;
-        }
-    }
-    if (this->numOpen == 0) {
-        std::cout << "No open ports found or host isn't up" << std::endl;
-    }
-}
-
-std::string Scan::getScanInfo() const {
-    std::time_t start = std::chrono::system_clock::to_time_t(startTime);
-    std::stringstream ss; 
-    ss << "Target Address: ";
-    ss << this->address; 
-    ss << "\tStart Time: ";
-    ss << ctime(&start);
-    ss << "\nTime Elapsed: "; 
-    ss << elapsedTime.count(); 
-    ss << "s";
-    ss << "\nPorts Open: "; 
-    ss << std::to_string(this->numOpen); 
-    ss << "\nPorts Closed: "; 
-    ss << std::to_string(this-> numClosed);
-    ss <<  "\n";
-    return ss.str();
-}
-
-std::string Scan::generatePortLog() const {
-    std::stringstream ss; 
-    if (this->numOpen == 0) {
-    ss << "No open ports found or host isn't up\n";
-    }
-    ss << "\n";
-    for (auto element : this->portMap)  {
-        ss << element.second.statusString() << "\n";
-    }
-    return ss.str();
 }
 
 bool Scan::addressOK() const {
--- src/Scan.h
+++ src/Scan.h
@@ -20,30 +20,13 @@
 
 class Scan {
 public:
-    Scan(const std::string address, const bool verbose);
+    Scan(const std::string address);
     ~Scan();
     bool addressOK() const;
-    void addPort(const int port);
     bool probe(const int port);
-    void printPorts();
-    void startScan();
-    std::string getScanInfo() const;
-    std::string generatePortLog() const;
-    std::string getAddress();
-    int getNumOpen();
-    int getNumClosed();
-    std::chrono::duration<double> getElapsedTime();
-    
 
 private:
     std::string address;
-    std::map<int, PortEntry> portMap;
-    std::chrono::system_clock::time_point startTime;
-    std::chrono::system_clock::time_point endTime;
-    std::chrono::duration<double> elapsedTime;
-    bool verbose;
-    int numOpen;
-    int numClosed;
     int sock;
     struct sockaddr_in server;
 };
